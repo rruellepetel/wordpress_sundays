d333bff1f3eb453e67c2acfd69954965a57642d6
rruellepetel <ruellepetelromain@gmail.com>
2017-05-23 15:04:28 +0200

    add get_categories()  and get_post()

--- index.php
+++ index.php
@@ -1,8 +1,27 @@
 <?php
 get_header();?>
 
+<?php
 
 
+$cats = get_categories();
+foreach ($cats as $cat) {
+    $args = array('category' => $cat->cat_ID);
+    $posts = get_posts($args);
+
+     foreach ($posts as $post) {
+        setup_postdata($post);
+        the_title('<h3>', '</h3>');
+        the_excerpt();
+        the_meta();
+
+        $mykey_values = get_post_custom_values('Price');
+        foreach ($mykey_values as $value) {
+        }
+    }
+}
+
+ ?>
 
 
 
