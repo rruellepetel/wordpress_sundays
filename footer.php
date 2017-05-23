aa971f586a2dc40aa61f0ace0dab69fcd13cff38
rruellepetel <ruellepetelromain@gmail.com>
2017-05-23 14:36:02 +0200

    initial commit

---
+++ footer.php
@@ -0,0 +1,40 @@
+<footer>
+        <nav>
+            <section>
+                <h4>Quick Links</h4>
+                <ul>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Careers</a></li>
+                </ul>
+            </section>
+            <section>
+                <h4>Contact Us</h4>
+                <ul>
+                    <li><a href="#"><span class="fa fa-twitter"></span>@californiatheme</a></li>
+                    <li><a href="#"><span class="fa fa-envelope"></span>support@smallvictories.co</a></li>
+                    <li><a href="#"><span class="fa fa-phone"></span>+1 917 (123) 4567</a></li>
+                </ul>
+            </section>
+            <section>
+                <h4>Newsletter</h4>
+                <form action="#">
+                    <input type="email" placeholder="Your Email ...">
+                    <button type="button" name="button">OK</button>
+                </form>
+            </section>
+            <section>
+                <h4>Follow Us</h4>
+                <ul>
+                    <li><a href="#"><span class="fa fa-pinterest"></span>Pinterest</a></li>
+                    <li><a href="#"><span class="fa fa-instagram"></span>Instagram</a></li>
+                    <li><a href="#"><span class="fa fa-vimeo"></span>Vimeo</a></li>
+                </ul>
+            </section>
+        </nav>
+    </footer>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
+    </script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"
+integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+crossorigin="anonymous"></script>
---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang=fr>
+<head>
+    <meta charset=UTF-8>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+ <link rel="stylesheet" href="<?php echo get_stylesheet_uri()?>">
+    <title>Sundays-Sass</title>
+</head>
+<body>
+    <header>
+        <a href="#"><i class="fa fa-search"></i></a>
+        <a href="#"><span class="fa fa-chevron-down"></span></a>
+        <nav>
+            <ul>
+                <li><a href="produit.html">Catalog</a></li>
+                <li><a href="contact.html">About Us</a></li>
+                <li><a href="panier.html">Stores</a></li>
+                <li><a href="#">Blog</a></li>
+            </ul>
+        </nav>
+
+        <section class="brand">
+            <h1>Sundays</h1>
+            <span class="slogan">We craft ocean friendly apparel</span>
+            <a href="#" class="btn">Browse Our Products</a>
+        </section>
+    </header>
